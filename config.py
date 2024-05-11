f6d4cd827b444b234e4d756374199ea320656621
mak12567 <148629684+mak12567@users.noreply.github.com>
2024-05-11 06:31:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
-import re, os, time
+import re
+import os
 
-id_pattern = re.compile(r'^.\d+$') 
+id_pattern = re.compile(r'^.\d+$')
 
-    # pyro client config
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+# Pyrogram client config
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMINS = []
+
+# Parsing admin IDs from environment variable and validating
+for x in os.environ.get("ADMINS", "").split():
+    try:
         ADMINS.append(int(x))
-except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")  
-# database config
-DB_NAME = os.environ.get("DB_NAME","Cluster0")    
-DB_URL  = os.environ.get("DB_URL","")
- 
-    
+    except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
 
+# Database config
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+DB_URL = os.environ.get("DB_URL", "")
